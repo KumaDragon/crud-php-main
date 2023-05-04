846fc13088ab6f473b586a0242e7b5b1b4ae85d6
Thales Sousa <62725292+KumaDragon@users.noreply.github.com>
2023-05-04 15:49:18 -0300

    construct 27/04
    adicionado o método construtor

--- classes/categoria.class.php
+++ classes/categoria.class.php
@@ -1,22 +1,45 @@
 <?php
+include_once("interface/crud.php");
 
-    include_once("../interfaces/crud.php");
-    
 class Categoria implements crud{
-    protected $id;
-    protected $nome;
+    private $id;
+    private $nome;
+
+    public function __construct($id=false){
+        if($id){
+            
+            $sql = "SELECT * FROM categoria WHERE id = ?";
+            $conexao = DB::conexao();
+            
+            $stmt = $conexao->prepare($sql);
+            $stmt->bindParam(1,$id, PDO::PARAM_INT);
+            $stmt->execute();
+            
+            foreach($stmt as $obj){
+                $this->setId($obj['id']);
+                $this->setNome($obj['nome']);
+            }
+
+            //$stmt = DB::conexao()->prepare($sql);
+        }
+    }
 
     public function setId($id){
-        $this->id = $id;
-    }
-    public function getId(){
-        return $this->id;
+        $this -> id = $id;
     }
     public function setNome($nome){
-        $this->nome = $nome;
+        $this -> nome = $nome;
+    }
+
+    public function getId(){
+        return $this -> id;
     }
     public function getNome(){
-        return $this->nome;
+        return $this -> nome;
     }
+
+    public function adicionar(){}
+    public function listar(){}
+    public function atualizar(){}
+    public function excluir(){}
 }
-?>
