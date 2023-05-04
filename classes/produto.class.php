0187ddc1a0df48e74e677e7c9bfc230cfcbf67f9
Thales Sousa <62725292+KumaDragon@users.noreply.github.com>
2023-05-04 15:51:14 -0300

    construct produto 27/04
    adicionado o método construtor em PRODUTO

--- classes/produto.class.php
+++ classes/produto.class.php
@@ -1,61 +1,71 @@
 <?php
-
-    include_once("interfaces/crud.php");
+include_once("interface/crud.php");
+include_once("db.class.php");
 
 class Produto implements crud{
-    protected $id;
-    protected $nome;
-    protected $categoria_id;
-    protected $preco;
-    protected $quant;
-
-    public function setId($id){
-        $this->id = $id;
-    }
-    public function getId(){
-        return $this->id;
-    }
-
-    public function setNome($nome){
-        $this->nome = $nome;
-    }
-    public function getNome(){
-        return $this->nome;
-    }
-    
-    public function setCategoria($categoria_id){
-        $this->categoria_id = $categoria_id;
-    }
-    public function getCategoria(){
-        return $this->categoria_id;
-    }
-
-    public function setPreco($preco){
-        $this->preco = $preco;
-    }
-    public function getPreco(){
-        return $this->preco;
-    }
-
-    public function setQuant($quant){
-        $this->quant = $quant;
-    }
-    public function getQuant(){
-        return $this->quant;
-    }
+    private $id;
+    private $categoria_id;
+    private $nome;
+    private $preco;
+    private $quantidade;
 
     public function __construct($id=false){
         if($id){
-            echo "Testando o construtor";
+            
+            $sql = "SELECT * FROM produtos WHERE id = ?";
+            $conexao = DB::conexao();
+            
+            $stmt = $conexao->prepare($sql);
+            $stmt->bindParam(1,$id, PDO::PARAM_INT);
+            $stmt->execute();
+            
+            foreach($stmt as $obj){
+                $this->setId($obj['id']);
+                $this->setCat($obj['categoria_id']);
+                $this->setNome($obj['nome']);
+                $this->setPreco($obj['preco']);
+                $this->setQuant($obj['quantidade']);
+            }
+
+            //$stmt = DB::conexao()->prepare($sql);
         }
     }
 
-    public function adicionar(){}    //C
-    public function listar(){}       //R
-    public function atualizar(){}    //U
-    public function excluir(){}      //D
+    #setters
+    public function setId($id){
+        $this -> id = $id;
+    }
+    public function setCat($categoria_id){
+        $this -> categoria_id = $categoria_id;
+    }
+    public function setNome($nome){
+        $this -> nome = $nome;
+    }
+    public function setPreco($preco){
+        $this -> preco = $preco;
+    }
+    public function setQuant($quantidade){
+        $this -> quantidade = $quantidade;
+    }
+    #getters
+    public function getId(){
+        return $this -> id;
+    }
+    public function getCat(){
+        return $this -> categoria_id;
+    }
+    public function getNome(){
+        return $this -> nome;
+    }
+    public function getPreco(){
+        return $this -> preco;
+    }
+    public function getQuant(){
+        return $this -> quantidade;
+    }
 
+        public function adicionar(){}
+        public function listar(){}
+        public function atualizar(){}
+        public function excluir(){}
 }
-
-
-?>
